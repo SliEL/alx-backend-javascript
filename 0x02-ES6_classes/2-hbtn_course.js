9af9f2a6cb234294d8091766c0a8f6ae4241c05e
Sliman Elmansouri <elmansourisliman@gmail.com>
2024-03-07 20:32:03 +0100

    Holberton Course Task

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
@@ -13,7 +13,6 @@ export default class HolbertonCourse {
     if (typeof name !== "string" && !(name instanceof String)) {
       throw new TypeError("Name must be a string");
     }
-
     this._name = name;
   }
 
@@ -37,7 +36,7 @@ export default class HolbertonCourse {
       !(students instanceof Array) ||
       !students.every((s) => typeof s === "string")
     ) {
-      throw new TypeError("Students must be am array of strings");
+      throw new TypeError("Students must be an array of strings");
     }
     this._students = students;
   }
